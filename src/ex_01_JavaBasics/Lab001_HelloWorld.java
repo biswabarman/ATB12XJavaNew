60ca4ca37c9b1c56f717dcc2140aefbb018f64f7
bjbarman18 <bjbarman18@gmail.com>
2025-05-24 11:15:22 +0530

    10 May 2025

--- src/ex_01_JavaBasics/Lab001_HelloWorld.java
+++ src/ex_01_JavaBasics/Lab001_HelloWorld.java
@@ -3,6 +3,6 @@ package ex_01_JavaBasics;
 public class Lab001_HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!!");
     }
 }
