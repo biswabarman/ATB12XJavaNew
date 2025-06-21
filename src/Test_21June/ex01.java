2ce6f5bfbd8b13609af41dccfce9a989f4800304
bjbarman18 <bjbarman18@gmail.com>
2025-06-21 11:54:38 +0530

    21 June test

---
+++ src/Test_21June/ex01.java
@@ -0,0 +1,23 @@
+package Test_21June;
+
+public class ex01 {
+    public static void main(String[] args) {
+        //  integer variable
+        int age = 25;
+
+        // double variable
+        double averageAge = 28.75;
+
+        // boolean variable
+        boolean isPresent = true;
+
+        // character variable
+        char section = 'A';
+
+        // Printing values of all variables
+        System.out.println("My Age: " + age);
+        System.out.println("Average age: " + averageAge);
+        System.out.println("Is Present: " + isPresent);
+        System.out.println("Section: " + section);
+    }
+}
