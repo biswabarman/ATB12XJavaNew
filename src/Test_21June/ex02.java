56bb981176dfd32f5197ddca6ee429c69060da85
bjbarman18 <bjbarman18@gmail.com>
2025-06-21 12:19:55 +0530

    21 June test

---
+++ src/Test_21June/ex02.java
@@ -0,0 +1,32 @@
+package Test_21June;
+
+public class ex02 {
+    public static void main(String[] args) {
+
+        // Arithmetic Operators
+
+        int num1 = 10;
+        int num2 = 5;
+
+        // Addition
+        int sum = num1 + num2;
+        System.out.println("Addition: " +num1 + " + " +num2 + " = " +sum);
+
+        // Subtraction
+        int difference = num1 - num2;
+        System.out.println("Subtraction: " +num1 + " - " +num2 + " = " +difference);
+
+        // Multiplication
+        int product = num1 * num2;
+        System.out.println("Multiplication: " +num1 + " * " +num2 + " = " +product);
+
+        // Division
+        int quotient = num1 / num2; // Integer division
+        System.out.println("Division (integer): " +num1 + " / " +num2 + " = " +quotient);
+
+        // Modulus
+        int remainder = num1 % num2;
+        System.out.println("Modulus: " +num1 + " % " +num2 + " = " +remainder);
+
+    }
+}
